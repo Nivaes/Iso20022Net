db1681b6e0718a6c9a3fca07b2d9c282363760c7
agimenez <andres.gimenez@outlook.com>
2019-06-11 17:48:39 +0200

    Include admi.

---
+++ Iso20022.UnitTest/UnitTest/Admi/AdmiM004SerializeTest.cs
@@ -0,0 +1,22 @@
+using System;
+using System.IO;
+using System.Xml.Serialization;
+using Xunit;
+
+namespace Iso20022.Admi.Test
+{
+    public class AdmiM004SerializeTest
+    {
+        [Theory]
+        [InlineData("./Resources/Admi/admi.004.001.01.xml")]
+        public void AdmiM004V00102(string fileName)
+        {
+            var serializder = new XmlSerializer(typeof(Iso20022.Contracts.AdmiM004V00101.Document));
+            using (var file = File.OpenRead(fileName))
+            {
+                var document = (Iso20022.Contracts.AdmiM004V00101.Document)serializder.Deserialize(file);
+                Assert.NotNull(document);
+            }
+        }
+    }
+}
